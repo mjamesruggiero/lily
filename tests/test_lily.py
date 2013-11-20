ec93d8e61269f5c02c4af8498dc368245944f6f9
mjamesruggiero <mjamesruggiero@gmail.com>
2013-11-19 18:18:18 -0800

    fix comments

--- tests/test_lily.py
+++ tests/test_lily.py
@@ -7,7 +7,7 @@ import numpy as np
 class TestLily(unittest.TestCase):
     def test_classify_0(self):
         """
-        lily - classify_0 returns the majority class as the prediction
+        core - classify_0 returns the majority class as the prediction
         """
         large_shape = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
         labels = ['foo', 'bar', 'bz', 'quux']
@@ -28,7 +28,7 @@ class TestLily(unittest.TestCase):
 
     def test_shannon_entropy(self):
         """
-        lily - calculate_shannon_entropy returns the shannon entropy
+        core - calculate_shannon_entropy returns the shannon entropy
         """
         my_data, labels = self.create_dataset()
         se = core.calculate_shannon_entropy(my_data)
@@ -36,7 +36,7 @@ class TestLily(unittest.TestCase):
 
     def test_split_dataset(self):
         """
-        lily - split_data_set breaks feature vectors on desired vals
+        core - split_data_set breaks feature vectors on desired vals
         """
         my_data, labels = self.create_dataset()
         returned = core.split_data_set(my_data, 0, 1)
@@ -44,7 +44,7 @@ class TestLily(unittest.TestCase):
 
     def test_choose_best_feature_to_split(self):
         """
-        lily - choose_best_feature_to_split discovers best feature
+        core - choose_best_feature_to_split discovers best feature
         """
         my_data, labels = self.create_dataset()
         returned = core.choose_best_feature_to_split(my_data)
@@ -68,7 +68,7 @@ class TestLily(unittest.TestCase):
         return posting_list, class_vector
 
     def test_create_vocabulary_list(self):
-        """lily - create_vocabulary_list creates... you know"""
+        """core - create_vocabulary_list creates... you know"""
         list_of_posts, list_classes = self.load_bayes_data_set()
         expected = ['cute', 'love', 'help', 'garbage',
                     'quit', 'food', 'problems', 'is',
@@ -81,7 +81,7 @@ class TestLily(unittest.TestCase):
         self.assertEqual(expected, core.create_vocabulary_list(list_of_posts))
 
     def test_set_of_words_to_vector(self):
-        """lily - set_of_words_to_vector turns words into bit vector"""
+        """core - set_of_words_to_vector turns words into bit vector"""
         vocabulary = ['you', 'hey', 'monkey',
                       'ape', 'gorilla', 'lump', 'chimpanzee']
         input_set = set({'hey', 'dude', 'chimpanzee'})
@@ -90,7 +90,7 @@ class TestLily(unittest.TestCase):
 
     def test_naive_bayes(self):
         """
-        lily - naive bayes can classify text based upon trained vocabulary
+        core - naive bayes can classify text based upon trained vocabulary
         """
         list_of_posts, list_classes = self.load_bayes_data_set()
         vocabulary = core.create_vocabulary_list(list_of_posts)
