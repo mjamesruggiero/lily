0f8a5f89eb986a74350650143c95a56be0fc86f0
mjamesruggiero <mjamesruggiero@gmail.com>
2013-12-18 16:56:02 -0800

    use one #load_dataset (and only one)

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -10,16 +10,15 @@ logging.basicConfig(level=logging.ERROR, format="%(lineno)d\t%(message)s")
 
 class TestRegression(unittest.TestCase):
 
-    def load_dataset(self):
+    def setUp(self):
         root_path = os.path.abspath(os.path.join(os.curdir, os.pardir))
-        file_path = "{0}/data/test_set.txt".format(root_path)
-        return utils.load_dataset(file_path)
+        self.test_file = "{0}/data/test_set.txt".format(root_path)
 
     def test_gradient_ascent(self):
         """
         regression - gradient_ascent returns accurate weights
         """
-        data_array, label_matrix = self.load_dataset()
+        data_array, label_matrix = utils.load_dataset(self.test_file)
         weights = regression.gradient_ascent(data_array, label_matrix)
         weights = [round(e, 2) for e in list(array(weights).reshape(-1,))]
 
