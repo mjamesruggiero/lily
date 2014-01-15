4be0297e15c8b33dd87356e5cb7e4404c0148a99
mjamesruggiero <mjamesruggiero@gmail.com>
2014-01-14 18:42:31 -0800

    backfill first AdaBoost test

---
+++ tests/test_ada_boost.py
@@ -0,0 +1,58 @@
+import unittest
+from context import lily
+from lily import ada_boost
+import numpy as np
+
+import logging
+logging.basicConfig(level=logging.INFO, format="%(lineno)d\t%(message)s")
+
+
+class TestAdaBoost(unittest.TestCase):
+
+    def setUp(self):
+        self.data_matrix = np.matrix([
+            [1.0, 2.2],
+            [2.0, 1.1],
+            [2.0, 1.0],
+            [1.0, 2.0]])
+        self.labels = [1.0, -1.0, -1.0, 1.0]
+
+    def test_ada_boost_stump_classify_partitions_lt(self):
+        """ada_boost - stump_classify partitions on less-than"""
+        i = 1
+        range_min = self.data_matrix[:, i].min()
+        threshold = (range_min * 2)
+        inequal = 'lt'
+        returned = ada_boost.stump_classify(self.data_matrix,
+                                            i,
+                                            threshold,
+                                            inequal)
+        expected = np.mat([1.0, -1.0, -1.0, -1.0])
+
+        delta_between_elements = returned - expected.T
+        self.assertFalse(delta_between_elements.any())
+
+    def test_ada_boost_stump_classify_partitions_gt(self):
+        """ada_boost - stump_classify partitions on greater-than"""
+        i = 1
+        range_min = self.data_matrix[:, i].min()
+        threshold = (range_min * 2)
+        inequal = 'gt'
+        returned = ada_boost.stump_classify(self.data_matrix,
+                                            i,
+                                            threshold,
+                                            inequal)
+        expected = np.mat([-1.0, 1.0, 1.0, 1.0])
+
+        delta_between_elements = returned - expected.T
+        self.assertFalse(delta_between_elements.any())
+
+    def test_aggregated_error_rate(self):
+        """ada_boost - aggregated error rate returns... error rate"""
+        estimates = np.mat([0.8, 0.4, 0.8, 0.4])
+        m = np.shape(self.data_matrix)[0]
+        returned = ada_boost.aggregated_error_rate(estimates, self.labels, m)
+        self.assertEqual(returned, 2.0)
+
+if __name__ == '__main__':
+    unittest.main()
